2fb3e420aadc18d10f71b307fe9c2ef676d3fcd7
uv-07 <63532070+uv-07@users.noreply.github.com>
2023-03-17 17:29:15 +0530

    static

--- express_/index.js
+++ express_/index.js
@@ -1,20 +1,24 @@
 const express = require('express')
+const path = require('path')
+// app.use(express.json())
+
+// app.get("/home", (req, res) => {
+//     console.log(req.query.name);
+//     res.send("This is home page")
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send("This is about page")
+// })
+
+// app.get('', (req, res) => {
+//     res.send("This is no page")
+// })
 
 const app = express();
-app.use(express.json())
 
-app.get("/home", (req, res) => {
-    console.log(req.query.name);
-    res.send("This is home page")
-})
+const publicPath = path.join(__dirname, 'public')
 
-app.get('/about', (req, res) => {
-    res.send("This is about page")
-})
-
-app.get('', (req, res) => {
-    res.send("This is no page")
-})
-
+app.use(express.static(publicPath))
 
 app.listen(3000);
